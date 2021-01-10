5f433ec64867c7336ffdcb6d024eed053e107038
jeongsuAn <dks9467@gmail.com>
2021-01-10 23:54:07 +0900

    add area, m2s

---
+++ area/area.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int width; //가로
+	int vertical; //세로
+	int area;
+
+	cin >> width >> vertical;
+	
+	area = width * vertical;
+
+	cout << area << endl;
+
+}
---
+++ m2s/m2s.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+	int min;
+	int sec;
+
+	cin >> min ;
+
+	sec = 60 * min ;
+
+	cout <<min<<"minutes is "<<sec<<" seconds."<<endl
+
+
+}
