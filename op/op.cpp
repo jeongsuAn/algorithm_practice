71950f61b151d5f69be70fa904c2f498a834bcef
jeongsuAn <dks9467@gmail.com>
2021-01-12 00:04:56 +0900

    finish op

--- op/op.cpp
+++ op/op.cpp
@@ -1,24 +1,52 @@
 #include <iostream>
 using namespace std;
 
-void op_add(int A,int B);
-/*
-min(int A,int B);
-mul(int A,int B);
-div(int A,int B);
-rem(int A,int B);
-*/
+void _add(int A,int B);
+void _min(int A,int B);
+void _mul(int A,int B);
+void _div(int A,int B);
+void _rem(int A,int B);
+
 int main(){
 	int a, b;
 	cin >> a >> b ;
 
-	op_add(a,b);
-//		<<min(a,b)<<mul(a,b)<<div(a,b)<<rem(a,b);
+	_add(a,b);
+	_min(a,b);
+	_mul(a,b);
+	_div(a,b);
+	_rem(a,b);
+
 	return 0;
 }
 
-void op_add(int A,int B){
+void _add(int A,int B){
 	int result;
 	result = A+B;
 	cout<<A<<"+"<<B<<"="<<result<<endl;
+}
+
+void _min(int A,int B){
+	int result;
+	result = A-B;
+	cout<<A<<"-"<<B<<"="<<result<<endl;
+}
+
+void _mul(int A,int B){
+	int result;
+	result = A*B;
+	cout<<A<<"*"<<B<<"="<<result<<endl;
+}
+
+void _div(int A,int B){
+	int result;
+	result = A/B;
+	cout<<A<<"/"<<B<<"="<<result<<endl;
+}
+
+void _rem(int A,int B){
+	int result;
+	result = A%B;
+	cout<<A<<"%"<<B<<"="<<result<<endl;
+
 }
