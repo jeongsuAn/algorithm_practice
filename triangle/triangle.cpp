2fac231175f96e925aaf931a635d3ea350950c5f
jeongsuAn <dks9467@gmail.com>
2021-01-12 01:03:46 +0900

    finish triangle

---
+++ triangle/triangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+float tri_area(float A, float B);
+
+int main(){
+	float base; //밑변
+	float height;
+	cin >> base >> height;
+	cout.precision(3);
+	cout << tri_area(base,height) << endl;
+}
+
+float tri_area(float A, float B){
+	float result;
+	result = A*B/2;
+	return result;
+}
