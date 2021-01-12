e022c893e372d4ac4566fe73ededacef09cedcd6
jeongsuAn <dks9467@gmail.com>
2021-01-12 23:57:02 +0900

    modify triangle

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -7,12 +7,14 @@ int main(){
 	float base; //밑변
 	float height;
 	cin >> base >> height;
+	
+	cout << fixed;
 	cout.precision(3);
 	cout << tri_area(base,height) << endl;
 }
 
 float tri_area(float A, float B){
 	float result;
-	result = A*B/2;
+	result = (A*B)/2;
 	return result;
 }
