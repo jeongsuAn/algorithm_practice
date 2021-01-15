804c6e4a5e7c0321ce4dd34635d53d73a889517c
jeongsuAn <dks9467@gmail.com>
2021-01-15 23:47:26 +0900

    koi_watch

---
+++ koi_watch/koi_watch.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int A,B,C,D;
+	int _hours,_min,_sec;
+	cin>> A,B,C,D;
+	
+	_hours=D/(60*60);
+	D%(60*60);
+	_min=D/60;
+	D=D%60;
+	_sec=D;
+
+	
+	
+
+
+}
