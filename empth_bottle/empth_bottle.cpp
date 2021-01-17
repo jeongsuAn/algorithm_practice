cf051a65789e8d36f77ae95a38e28ed26be10cef
jeongsuAn <dks9467@gmail.com>
2021-01-17 23:43:32 +0900

    finish empth_bottle

--- empth_bottle/empth_bottle.cpp
+++ empth_bottle/empth_bottle.cpp
@@ -5,6 +5,7 @@ int main(){
 	int first_beverage;
 	int first_rem;
 	int second_rem;
+	int total_rem;
 	int event_beverage;
 	int total_beverage;
 
@@ -13,22 +14,23 @@ int main(){
 	event_beverage=first_beverage/4;
 
 	//첫번째 이벤트 참가시 빈병개수
-	first_rem=(first_beverage%4)+event_beverage;
+	first_rem = first_beverage%4;
 	
 	//첫번째 이벤트 참가시 마시는 음료 수
 	total_beverage=first_beverage+event_beverage;
 	
 	
 	//두번째 이벤트 참가
-	event_beverage=first_rem/4;
+	second_rem=event_beverage%4;
+	event_beverage=event_beverage/4;
 	
 	//두번째 이벤트 참가시 빈병개수
-	second_rem=first_rem%4+event_beverage;
+	total_rem=first_rem+second_rem;
 	
 	//두번째 이벤트 참가시 마시는 총 음료수
 	total_beverage=total_beverage+event_beverage;
 
-	cout<<total_beverage<<" "<<second_rem<<endl;
+	cout<<total_beverage<<" "<<total_rem<<endl;
 	
 	return 0;
 
