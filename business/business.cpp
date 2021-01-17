48a6a0981bcef89fa61ff37f2078e119bcf32839
jeongsuAn <dks9467@gmail.com>
2021-01-18 00:14:54 +0900

    finish business

--- business/business.cpp
+++ business/business.cpp
@@ -2,16 +2,17 @@
 using namespace std;
 
 int main(){
-	int prime_cost,cost,counterfeit_coin,change;
+	int prime_cost,cost,fake_money,change;
+	int receive_money,real_money;
 	int total_damage;
 
-	cin>>prime_cost>>cost>>counterfeit_coin>>change;
-	
-	total_damage=prime_cost-cost+counterfeit_coin+change;
+	cin>>prime_cost>>cost>>fake_money>>change;
+	receive_money=cost+change;
+	real_money=receive_money-fake_money;
+	real_money=real_money-change;
+	total_damage=prime_cost-real_money;
 
-	cout>>total_damage>>endl;
-	
+	cout<<total_damage<<endl;
 
-
-
+	return 0;
 }
