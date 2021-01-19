e11baab738f951a7fb8e15ea30593c2e047e95e5
jeongsuAn <dks9467@gmail.com>
2021-01-19 21:50:45 +0900

    finish paintblock

---
+++ paintblock/paintblock.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int width,length,hight;
+	int result;	
+	cin>>width>>length>>hight;
+
+	result=(width-2+length-2+hight-2)*4;
+
+	cout<<result<<endl;
+	
+
+
+	return 0;
+
+}
