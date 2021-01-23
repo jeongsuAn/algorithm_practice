cdabb155a796a6f9d24164939314f934f5517bd2
jeongsuAn <dks9467@gmail.com>
2021-01-23 23:58:39 +0900

    finish flr

---
+++ flr/flr.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	int flr, num;
+	int flr_num = 0;
+
+	cin>>num;
+
+	for(flr=0; flr_num<num; flr++){
+		flr_num=(pow(2,flr)-2);		
+//		cout<<"flr_num = "<<flr_num<<endl;
+//		cout<<"flr = "<<flr<<endl;
+	}	
+
+	flr--;
+	cout<<flr<<endl;
+
+
+}
+
