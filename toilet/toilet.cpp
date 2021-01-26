1f4c5f67bce97ed7d5dbace877885344d017cfbd
jeongsuAn <dks9467@gmail.com>
2021-01-26 21:55:32 +0900

    finish toilet

---
+++ toilet/toilet.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int N;
+	int min_man,max_man;
+	cin>>N;
+
+	max_man=(N+1)/2;
+	min_man=(N+2)/3;
+	
+	cout<<max_man<<" "<<min_man<<endl;
+
+}
